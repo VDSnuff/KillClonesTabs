54f789b338fd35fbc187fae570df3dd1c3c7ef24
Viacheslav Dovnich <v.dovnich@gmail.com>
2016-11-02 02:17:44 +0100

    Comparison of two arrays, completed.

--- popup.js
+++ popup.js
@@ -1,14 +1,31 @@
-document.getElementById('btnTarget').onclick = testFunc;
+var tabListBefor = [];
+var tabListAfter = [];
 
-function testFunc() {
+//Get list of all opened tabs.
+function getTabsListFunc() {
     chrome.windows.getAll({
         populate: true
     }, function(windows) {
         windows.forEach(function(window) {
             window.tabs.forEach(function(tab) {
-                //collect all of the urls here, I will just log them instead
-                console.log(tab.url);
+                //Test
+                //console.log(tab.url + "tabs id: " + tab.id);
+                tabListBefor.push({
+                    tabUrl: tab.url,
+                    tabId: tab.id
+                });
             });
         });
     });
+
+    for (var i = 0; i < tabListBefor.length; i++) {
+        for ( var j = tabListBefor.length - 1; j > 0; j--) {
+            if (tabListBefor[i].tabUrl == tabListBefor[j].tabUrl && i != j) {
+                tabListAfter.push(tabListBefor[j].tabUrl.toString());
+            }
+        }
+    }
+    console.log(tabListAfter);
 }
+//Linking with getTabsListFunc function
+document.getElementById('btnTarget').onclick = getTabsListFunc;
