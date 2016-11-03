1375756ec3cf23713c50975fd31945a364201917
Viacheslav Dovnich <v.dovnich@gmail.com>
2016-11-03 01:13:53 +0100

    Highlight the clone tabs function completed

--- popup.js
+++ popup.js
@@ -1,31 +1,94 @@
-var tabListBefor = [];
+var tabListBeforOne = [];
+var tabListBeforTwo = [];
 var tabListAfter = [];
+var tabIndex = [];
 
-//Get list of all opened tabs.
-function getTabsListFunc() {
+function highlightClonesFunc() {
+
     chrome.windows.getAll({
         populate: true
     }, function(windows) {
         windows.forEach(function(window) {
             window.tabs.forEach(function(tab) {
                 //Test
-                //console.log(tab.url + "tabs id: " + tab.id);
-                tabListBefor.push({
+                tabListBeforOne.push({
                     tabUrl: tab.url,
-                    tabId: tab.id
+                    tabId: tab.id,
+                    tabIndex: tab.index
+                });
+                tabListBeforTwo.push({
+                    tabUrl: tab.url,
+                    tabId: tab.id,
+                    tabIndex: tab.index
                 });
             });
         });
-    });
 
-    for (var i = 0; i < tabListBefor.length; i++) {
-        for ( var j = tabListBefor.length - 1; j > 0; j--) {
-            if (tabListBefor[i].tabUrl == tabListBefor[j].tabUrl && i != j) {
-                tabListAfter.push(tabListBefor[j].tabUrl.toString());
+        for (var i = 0; i < tabListBeforOne.length; i++) {
+            for (var j = 0; j < tabListBeforTwo.length; j++) {
+                if (tabListBeforOne[i].tabUrl == tabListBeforTwo[j].tabUrl && tabListBeforOne[i].tabId != tabListBeforTwo[j].tabId) {
+                    tabListAfter.push(tabListBeforTwo[j]);
+                }
             }
         }
-    }
-    console.log(tabListAfter);
+
+        //Test 
+        //console.log(tabListAfter);
+
+        for (var e = 0; e < tabListAfter.length; e++) {
+            tabIndex.push(tabListAfter[e].tabIndex);
+        }
+
+        chrome.tabs.highlight({
+            tabs: tabIndex
+        });
+    });
 }
+
 //Linking with getTabsListFunc function
-document.getElementById('btnTarget').onclick = getTabsListFunc;
+document.getElementById('btnTarget').onclick = highlightClonesFunc;
+
+
+function killClonesFunc (){
+    
+     chrome.windows.getAll({
+        populate: true
+    }, function(windows) {
+        windows.forEach(function(window) {
+            window.tabs.forEach(function(tab) {
+                //Test
+                tabListBeforOne.push({
+                    tabUrl: tab.url,
+                    tabId: tab.id,
+                    tabIndex: tab.index
+                });
+                tabListBeforTwo.push({
+                    tabUrl: tab.url,
+                    tabId: tab.id,
+                    tabIndex: tab.index
+                });
+            });
+        });
+
+        for (var i = 0; i < tabListBeforOne.length; i++) {
+            for (var j = 0; j < tabListBeforTwo.length; j++) {
+                if (tabListBeforOne[i].tabUrl == tabListBeforTwo[j].tabUrl && tabListBeforOne[i].tabId != tabListBeforTwo[j].tabId) {
+                    tabListAfter.push(tabListBeforTwo[j]);
+                }
+            }
+        }
+
+        //Test 
+        //console.log(tabListAfter);
+
+        for (var e = 0; e < tabListAfter.length; e++) {
+            tabIndex.push(tabListAfter[e].tabIndex);
+        }
+
+        chrome.tabs.remove([2311]);
+    });
+    
+}
+
+//Linking with killClonesFunc function
+document.getElementById('btnKill').onclick = killClonesFunc;
