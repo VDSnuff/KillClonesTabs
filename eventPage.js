0566fc14ced9361b1d6f24c22f9f357389666e1d
Viacheslav <v.dovnich@gmail.com>
2025-11-11 19:59:43 +0100

    refactor: Share code with ES modules

--- eventPage.js
+++ eventPage.js
@@ -1,22 +1,4 @@
-// A more efficient way to find duplicate tabs.
-// Returns an array of duplicate tabs. The first tab with a given URL is not included.
-async function findDuplicateTabs() {
-    const tabs = await chrome.tabs.query({ windowType: 'normal' });
-    const urlMap = new Map();
-    const duplicates = [];
-
-    for (const tab of tabs) {
-        if (tab.url) {
-            if (urlMap.has(tab.url)) {
-                // The tab in the map is the "original", this one is a duplicate
-                duplicates.push(tab);
-            } else {
-                urlMap.set(tab.url, tab);
-            }
-        }
-    }
-    return duplicates;
-}
+import { findDuplicateTabs } from './utils.js';
 
 async function checkTabs() {
     const duplicateTabs = await findDuplicateTabs();
